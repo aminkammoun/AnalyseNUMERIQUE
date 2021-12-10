1e09232bc96d311e4ff63b9369db858248206901
aminkammoun <aminekammoun55@gmail.com>
2021-12-10 22:03:26 +0100

    auto push at 2021-12-10 22:03:26

--- commit.py
+++ commit.py
@@ -2,4 +2,4 @@ import subprocess
 import datetime
 subprocess.call(["git", "add", "."])
 subprocess.call(["git", "commit", "-m", "auto push at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")])
-subprocess.call(["git", "push","origin","master"])
+subprocess.call(["git", "push","origin master"])
